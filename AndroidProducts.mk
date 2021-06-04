ff2af59ee205febdb062574d4cc9f036e997d244
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-04 17:26:36 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1805.mk
+    $(LOCAL_DIR)/cherish_RMX1805.mk
 
-COMMON_LUNCH_CHOICES := \
-    lineage_RMX1805-user \
-    lineage_RMX1805-userdebug \
-    lineage_RMX1805-eng
