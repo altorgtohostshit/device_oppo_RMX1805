d3324af8961a0b7a78efff1667b0414a091fad97
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-05 05:58:42 +0530

    Update and rename aosp_RMX1805.mk to evolution_RMX1805.mk

--- evolution_RMX1805.mk
+++ evolution_RMX1805.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := aosp_RMX1805
+PRODUCT_NAME := evolution_RMX1805
 PRODUCT_MODEL := realme 2
 
 # PRODUCT_GMS_CLIENTID_BASE := android-oppo
@@ -25,9 +25,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1805"
 
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
+WITH_GAPPS := true
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aosp.maintainer=Abhinav
+    ro.evolution.maintainer=Abhinav
