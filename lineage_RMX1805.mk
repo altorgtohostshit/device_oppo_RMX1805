2469304bc6d52f2cc45b3a09b828d403325cb486
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-07 04:50:58 +0530

    Update and rename arrow_RMX1805.mk to lineage_RMX1805.mk

--- lineage_RMX1805.mk
+++ lineage_RMX1805.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
@@ -14,29 +14,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := arrow_RMX1805
+PRODUCT_NAME := lineage_RMX1805
 PRODUCT_MODEL := realme 2
 
 # PRODUCT_GMS_CLIENTID_BASE := android-oppo
 TARGET_VENDOR := oppo
 TARGET_VENDOR_PRODUCT_NAME := RMX1805
 TARGET_GAPPS_ARCH := arm64
-
-
-
-
-
 TARGET_INCLUDE_PIXEL_CHARGER := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1805"
 
 TARGET_GAPPS_ARCH := arm64
 WITH_GAPPS := true
-TARGET_OPLAUNCHER := true
-SHIP_LAWNCHAIR := true
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Maintainer
-DEVICE_MAINTAINER := AbhinavSama
+DEVICE_MAINTAINER :=AbhinavSama
