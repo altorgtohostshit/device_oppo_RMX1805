c810acae5b6c99a7f8bfa967cc884258b97453df
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-06 10:12:59 +0530

    Update lineage_RMX1805.mk

--- lineage_RMX1805.mk
+++ lineage_RMX1805.mk
@@ -3,7 +3,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/kangos/config/common.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := lineage_RMX1805
+PRODUCT_NAME := kangos_RMX1805
 PRODUCT_MODEL := realme 2
 
 # PRODUCT_GMS_CLIENTID_BASE := android-oppo
@@ -25,11 +25,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 TARGET_GAPPS_ARCH := arm64
 WITH_GAPPS := true
+TARGET_OPLAUNCHER := true
+SHIP_LAWNCHAIR := true
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Maintainer
-DEVICE_MAINTAINER := "Abhinav Sama"
+DEVICE_MAINTAINER := "Abhinav_Sama"
 ro.lineage.maintainer=Abhinav_Sama
 
