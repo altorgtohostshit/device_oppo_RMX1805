b3578d4da185f5870953c7d841b27aaff72cca09
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-07 13:30:36 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1805.mk
-
-COMMON_LUNCH_CHOICES := \
-    lineage_RMX1805-user \
-    lineage_RMX1805-userdebug \
-    lineage_RMX1805-eng
+    $(LOCAL_DIR)/aosp_RMX1805.mk
