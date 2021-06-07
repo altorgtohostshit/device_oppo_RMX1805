f45838ddcd211f3f98d6a5b3f511f002b38cac46
Abhinav Verma <79688470+Abhinavftp@users.noreply.github.com>
2021-06-07 13:29:26 +0530

    Update and rename lineage_RMX1805.mk to aosp_RMX1805.mk

--- aosp_RMX1805.mk
+++ aosp_RMX1805.mk
@@ -3,10 +3,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit from RMX1805 device
 $(call inherit-product, device/oppo/RMX1805/device.mk)
+
+# Inherit GMS, Pixel Features, and Modules.
+-include vendor/google/gms/config.mk
+
+
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
@@ -14,21 +19,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk
 PRODUCT_BRAND := oppo
 PRODUCT_DEVICE := RMX1805
 PRODUCT_MANUFACTURER := oppo
-PRODUCT_NAME := lineage_RMX1805
+PRODUCT_NAME := aosp_RMX1805
 PRODUCT_MODEL := realme 2
 
 # PRODUCT_GMS_CLIENTID_BASE := android-oppo
 TARGET_VENDOR := oppo
+TARGET_USE_JELLY := true
 TARGET_VENDOR_PRODUCT_NAME := RMX1805
-TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_PIXEL_CHARGER := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME="RMX1805"
 
-TARGET_GAPPS_ARCH := arm64
-WITH_GAPPS := true
-TARGET_INCLUDE_STOCK_ARCORE := false
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+#TARGET_GAPPS_ARCH := arm64
+#WITH_GAPPS := true
+#TARGET_INCLUDE_STOCK_ARCORE := false
+#TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Maintainer
